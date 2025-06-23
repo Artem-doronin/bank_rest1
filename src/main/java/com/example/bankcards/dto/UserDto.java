6465fadc20846b691854102a3e8c7a79f6932879
Artem Doronin <doronintema2018@yandex.ru>
2025-06-23 22:22:40 +0500

    test created 4

--- src/main/java/com/example/bankcards/dto/UserDto.java
+++ src/main/java/com/example/bankcards/dto/UserDto.java
@@ -2,13 +2,15 @@ package com.example.bankcards.dto;
 
 import com.example.bankcards.entity.UserStatus;
 import lombok.Builder;
+
 import java.util.Set;
+
 @Builder
-public record UserDto (
+public record UserDto(
         Long id,
         String username,
         Set<Long> rolesId,
         Set<Long> cardsId,
         UserStatus status
-){
+) {
 }
--- src/main/java/com/example/bankcards/dto/UserStatusUpdateRequest.java
+++ src/main/java/com/example/bankcards/dto/UserStatusUpdateRequest.java
@@ -2,19 +2,17 @@ package com.example.bankcards.dto;
 
 import com.example.bankcards.entity.UserStatus;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserStatusUpdateRequest {
     @NotNull(message = "Status must be provided")
     private UserStatus status;
 
-    public UserStatusUpdateRequest(UserStatus status) {
-        this.status = status;
-    }
-
-    public UserStatusUpdateRequest() {
-    }
 }
