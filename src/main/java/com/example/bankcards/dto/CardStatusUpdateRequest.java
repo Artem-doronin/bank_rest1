32b5e28b8c5c527f6f9ecf9e95b8ec0b35cd40a3
Artem Doronin <doronintema2018@yandex.ru>
2025-06-23 14:09:19 +0500

    test created 2

--- src/main/java/com/example/bankcards/dto/CardStatusUpdateRequest.java
+++ src/main/java/com/example/bankcards/dto/CardStatusUpdateRequest.java
@@ -2,9 +2,11 @@ package com.example.bankcards.dto;
 
 import com.example.bankcards.entity.CardStatus;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class CardStatusUpdateRequest {
     @NotNull(message = "Status must be provided")
     private CardStatus status;
--- src/test/java/com/example/bankcards/service/CardImplServiceTest.java
+++ src/test/java/com/example/bankcards/service/CardImplServiceTest.java
@@ -2,11 +2,16 @@ package com.example.bankcards.service;
 
 import com.example.bankcards.dto.CardCreateRequest;
 import com.example.bankcards.dto.CardResponse;
+import com.example.bankcards.dto.CardStatusUpdateRequest;
+import com.example.bankcards.dto.TransferRequest;
+import com.example.bankcards.dto.TransferResponse;
 import com.example.bankcards.entity.Card;
 import com.example.bankcards.entity.CardStatus;
 import com.example.bankcards.entity.User;
 import com.example.bankcards.exception.CardAlreadyExistsException;
+import com.example.bankcards.exception.CardBlockedException;
 import com.example.bankcards.exception.CardNotFoundException;
+import com.example.bankcards.exception.InsufficientFundsException;
 import com.example.bankcards.exception.InvalidCardIdException;
 import com.example.bankcards.exception.InvalidUserIdException;
 import com.example.bankcards.exception.UserNotFoundException;
@@ -27,11 +32,13 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -53,7 +60,6 @@ public class CardImplServiceTest {
     public void testPositiveCreateCard() {
         CardCreateRequest request = createCardRequest();
         User user = createUser();
-
 
         when(cardRepository.existsByCardNumber(anyString())).thenReturn(false);
         when(userRepository.findById(1L)).thenReturn(Optional.of(user));
@@ -78,8 +84,8 @@ public class CardImplServiceTest {
         CardResponse response1 = cardService.getCardById(1L, true);
 
         assertEquals(cardResponse.getId(), response1.getId());
-
     }
+
     @Test
     public void testPositiveGetCardByUserId() {
         User user = createUser();
@@ -91,12 +97,114 @@ public class CardImplServiceTest {
         List<CardResponse> list = cardService.getCardsByUserId(1L);
 
         assertEquals(list.size(), 1);
-        assertEquals(list.get(0).getId(),card.getId());
+        assertEquals(list.get(0).getId(), card.getId());
     }
 
+    @Test
+    public void testPositiveUpdateCardStatus() {
+        CardStatusUpdateRequest cardStatusUpdateRequest = createCardStatusUpdateRequest();
 
+        when(cardRepository.findById(1L)).thenReturn(Optional.of(createCard()));
+        doNothing().when(securityAccessService).checkUserAccess(1L);
 
+        cardService.updateCardStatus(1L, cardStatusUpdateRequest);
 
+        verify(cardRepository).save(cardCaptor.capture());
+        Card capturedCard = cardCaptor.getValue();
+
+        assertEquals(capturedCard.getStatus(), cardStatusUpdateRequest.getStatus());
+
+    }
+
+    @Test
+    public void testPositiveTransferFunds() {
+        TransferRequest request = createTransferRequest();
+        Card sourceCard = createCard();
+        sourceCard.setBalance(new BigDecimal("500.00"));
+        Card destinationCard = createCard();
+        destinationCard.setId(2L);
+        destinationCard.setBalance(new BigDecimal("100.00"));
+
+        when(cardRepository.findById(request.getSourceCardId())).thenReturn(Optional.of(sourceCard));
+        when(cardRepository.findById(request.getDestinationCardId())).thenReturn(Optional.of(destinationCard));
+        doNothing().when(securityAccessService).checkUserAccess(1L);
+
+        TransferResponse transferResponse = cardService.transferFunds(request);
+
+        verify(cardRepository, times(2)).save(cardCaptor.capture());
+
+        List<Card> capturedCards = cardCaptor.getAllValues();
+
+        assertEquals(0, new BigDecimal(400).compareTo(capturedCards.get(0).getBalance()));
+        assertEquals(1L, capturedCards.get(0).getId());
+
+        assertEquals(0, new BigDecimal(200).compareTo(capturedCards.get(1).getBalance()));
+        assertEquals(2L, capturedCards.get(1).getId());
+
+        assertNotNull(transferResponse);
+    }
+
+    @Test
+    public void testNegativeTransferFundsSourceNoActive() {
+        TransferRequest request = createTransferRequest();
+        Card sourceCard = createCard();
+        sourceCard.setBalance(new BigDecimal("500.00"));
+        sourceCard.setStatus(CardStatus.BLOCKED);
+        Card destinationCard = createCard();
+        destinationCard.setId(2L);
+
+        when(cardRepository.findById(request.getSourceCardId())).thenReturn(Optional.of(sourceCard));
+        when(cardRepository.findById(request.getDestinationCardId())).thenReturn(Optional.of(destinationCard));
+        doNothing().when(securityAccessService).checkUserAccess(1L);
+
+        assertThrows(CardBlockedException.class, () -> cardService.transferFunds(request));
+    }
+
+    @Test
+    public void testNegativeTransferFundsInsufficientFunds() {
+        TransferRequest request = createTransferRequest();
+        Card sourceCard = createCard();
+        sourceCard.setStatus(CardStatus.ACTIVE);
+        sourceCard.setBalance(new BigDecimal("50.00"));
+        Card destinationCard = createCard();
+        destinationCard.setId(2L);
+        destinationCard.setStatus(CardStatus.ACTIVE);
+
+        when(cardRepository.findById(request.getSourceCardId())).thenReturn(Optional.of(sourceCard));
+        when(cardRepository.findById(request.getDestinationCardId())).thenReturn(Optional.of(destinationCard));
+        doNothing().when(securityAccessService).checkUserAccess(1L);
+
+        assertThrows(InsufficientFundsException.class, () -> cardService.transferFunds(request));
+    }
+
+    @Test
+    public void testNegativeTransferFundsDestinationNoActive() {
+        TransferRequest request = createTransferRequest();
+        Card sourceCard = createCard();
+        sourceCard.setStatus(CardStatus.ACTIVE);
+
+        Card destinationCard = createCard();
+        destinationCard.setId(2L);
+        destinationCard.setStatus(CardStatus.BLOCKED);
+
+        when(cardRepository.findById(request.getSourceCardId())).thenReturn(Optional.of(sourceCard));
+        when(cardRepository.findById(request.getDestinationCardId())).thenReturn(Optional.of(destinationCard));
+        doNothing().when(securityAccessService).checkUserAccess(1L);
+
+        assertThrows(CardBlockedException.class, () -> cardService.transferFunds(request));
+    }
+
+    @Test
+    public void testNegativeUpdateCardStatusCardIdIsNegative() {
+        assertThrows(InvalidCardIdException.class,
+                () -> cardService.updateCardStatus(-1L, createCardStatusUpdateRequest()));
+    }
+
+    @Test
+    public void testNegativeUpdateCardStatusCardIdNotFound() {
+        assertThrows(InvalidCardIdException.class,
+                () -> cardService.updateCardStatus(null, createCardStatusUpdateRequest()));
+    }
 
     @Test
     public void testNegativeGetCardByIdCardNotFound() {
@@ -165,6 +273,7 @@ public class CardImplServiceTest {
     private Card createCard() {
         return Card.builder()
                 .id(1L)
+                .status(CardStatus.ACTIVE)
                 .owner(User.builder()
                         .id(1L)
                         .build())
@@ -181,4 +290,18 @@ public class CardImplServiceTest {
                 .build();
     }
 
+    private CardStatusUpdateRequest createCardStatusUpdateRequest() {
+        return CardStatusUpdateRequest.builder()
+                .status(CardStatus.BLOCKED)
+                .build();
+    }
+
+    private TransferRequest createTransferRequest() {
+        return TransferRequest.builder()
+                .sourceCardId(1L)
+                .destinationCardId(2L)
+                .amount(new BigDecimal(100))
+                .description("test")
+                .build();
+    }
 }
